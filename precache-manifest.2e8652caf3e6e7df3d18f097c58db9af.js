91c6f37efdaaf8e9ffdf130e8e4b1d5842e939b0
sankalpnarayan27 <56334939+sankalpnarayan27@users.noreply.github.com>
2019-12-05 14:58:32 +0530

    Updates

---
+++ precache-manifest.2e8652caf3e6e7df3d18f097c58db9af.js
@@ -0,0 +1,195 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "644af4047a5ed147d2fd7daaf9680f8e",
+    "url": "/INDUCTION_NEW/index.html"
+  },
+  {
+    "revision": "bfb20577e074eb084aed",
+    "url": "/INDUCTION_NEW/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "37f9e8a7df3c8d6dbb2e",
+    "url": "/INDUCTION_NEW/static/css/main.76312a35.chunk.css"
+  },
+  {
+    "revision": "bfb20577e074eb084aed",
+    "url": "/INDUCTION_NEW/static/js/2.7a65517a.chunk.js"
+  },
+  {
+    "revision": "37f9e8a7df3c8d6dbb2e",
+    "url": "/INDUCTION_NEW/static/js/main.950d5dc7.chunk.js"
+  },
+  {
+    "revision": "26c5e17fdfc288df5220",
+    "url": "/INDUCTION_NEW/static/js/runtime-main.23f6be59.js"
+  },
+  {
+    "url": "/INDUCTION_NEW/static/media/05101d77995fd17eed5a6340b0ec2e89.05101d77.png"
+  },
+  {
+    "revision": "66eeab741bd3553f5d614bd60c62c457",
+    "url": "/INDUCTION_NEW/static/media/07f5c25f4997aedd3416ca7527c084e3.66eeab74.png"
+  },
+  {
+    "revision": "0d7d174616f3fa83655cb9cf63f2a9fb",
+    "url": "/INDUCTION_NEW/static/media/19-198388_github-logo-github-git-hd-png-download.0d7d1746.png"
+  },
+  {
+    "url": "/INDUCTION_NEW/static/media/2dad917621850da1a2e1f76e1aa9650c.3be058e9.png"
+  },
+  {
+    "url": "/INDUCTION_NEW/static/media/40aad5e2d2d264aeb7e8f785f27c237c.40aad5e2.png"
+  },
+  {
+    "revision": "e8a66ccf75d58a59361146d9001bd4d9",
+    "url": "/INDUCTION_NEW/static/media/432-4329261_jira-software-logo-jira-logo-hd-png-download.e8a66ccf.png"
+  },
+  {
+    "revision": "78a3a5baf14fd3cef65ca2347f2f8f08",
+    "url": "/INDUCTION_NEW/static/media/58482879cef1014c0b5e49e8.78a3a5ba.png"
+  },
+  {
+    "revision": "cd0c78940438c039ec1dabeb68b3f339",
+    "url": "/INDUCTION_NEW/static/media/5cb480cd5f1b6d3fbadece79.cd0c7894.png"
+  },
+  {
+    "revision": "928023ff3989615feaa18362854f1afc",
+    "url": "/INDUCTION_NEW/static/media/618246_outlook_2000x2000.928023ff.png"
+  },
+  {
+    "url": "/INDUCTION_NEW/static/media/7a9e366ef62ea6b9f565536c2b3945b3.7a9e366e.svg"
+  },
+  {
+    "revision": "6870fcab66ce7311eaa43b2c320db968",
+    "url": "/INDUCTION_NEW/static/media/Group_161.6870fcab.svg"
+  },
+  {
+    "revision": "34515a00aaf8e3fdb872aa5b5d1de83d",
+    "url": "/INDUCTION_NEW/static/media/Xebia_Logo_Purple_RGB-LG.34515a00.png"
+  },
+  {
+    "revision": "ca80b26cb67c550c05141d9c600ac806",
+    "url": "/INDUCTION_NEW/static/media/Xebia_Logo_Purple_RGB-LG.ca80b26c.svg"
+  },
+  {
+    "revision": "12729db6f4ca65a5aff7a4e01b41ce02",
+    "url": "/INDUCTION_NEW/static/media/Xebia_Logo_White_CMYK-LG.12729db6.png"
+  },
+  {
+    "url": "/INDUCTION_NEW/static/media/af3555d47a3d4463c519797414d6e94a.af3555d4.png"
+  },
+  {
+    "revision": "a88bffaddd40a96b4e19967bab79ed5e",
+    "url": "/INDUCTION_NEW/static/media/attach.a88bffad.svg"
+  },
+  {
+    "revision": "53c6bd6e6a48e5c1ec1f9d185913a7f0",
+    "url": "/INDUCTION_NEW/static/media/born.53c6bd6e.svg"
+  },
+  {
+    "revision": "6c14fc12616eeccdbdef823617f7c1c8",
+    "url": "/INDUCTION_NEW/static/media/calendar.6c14fc12.svg"
+  },
+  {
+    "revision": "0037960e31577b17dd660fdb4edf5936",
+    "url": "/INDUCTION_NEW/static/media/connection.0037960e.svg"
+  },
+  {
+    "revision": "e5d8333a1c9cc2887e251468fcde3892",
+    "url": "/INDUCTION_NEW/static/media/customer-support.e5d8333a.svg"
+  },
+  {
+    "revision": "a7cb2873ef4cf08a75476b44a4699f47",
+    "url": "/INDUCTION_NEW/static/media/day to day.a7cb2873.png"
+  },
+  {
+    "revision": "c99724dbd71d69d9c1ebe63b9ecef7cc",
+    "url": "/INDUCTION_NEW/static/media/day to day@2x.c99724db.png"
+  },
+  {
+    "revision": "5ac4a493a07b00bc2b3cbb8dc222a801",
+    "url": "/INDUCTION_NEW/static/media/diamond.5ac4a493.svg"
+  },
+  {
+    "url": "/INDUCTION_NEW/static/media/eeceb7aef2ba89e5fd7f873ad2641fce.eeceb7ae.png"
+  },
+  {
+    "url": "/INDUCTION_NEW/static/media/f9097e72e7e423449defaf80fb8e35c8.f9097e72.png"
+  },
+  {
+    "revision": "8a0ed65b5bf53b23c685b4ce0b2bad60",
+    "url": "/INDUCTION_NEW/static/media/hourglass.8a0ed65b.svg"
+  },
+  {
+    "revision": "3331216761134cf9035f3b49983dcdf7",
+    "url": "/INDUCTION_NEW/static/media/lifeAtXebia.33312167.svg"
+  },
+  {
+    "revision": "7b9874edcdcd365947de7aee739bf800",
+    "url": "/INDUCTION_NEW/static/media/links.7b9874ed.txt"
+  },
+  {
+    "revision": "2e9d35984a51be31504bf872c99dfd45",
+    "url": "/INDUCTION_NEW/static/media/meeting.2e9d3598.svg"
+  },
+  {
+    "revision": "6794f01bc2b2e57328271b8ae1346af8",
+    "url": "/INDUCTION_NEW/static/media/policy.6794f01b.svg"
+  },
+  {
+    "revision": "f1f10eb2d62722d413fb24ff4ed9c422",
+    "url": "/INDUCTION_NEW/static/media/quality.f1f10eb2.svg"
+  },
+  {
+    "revision": "84a6e7b560c532f4f2584c19f1764320",
+    "url": "/INDUCTION_NEW/static/media/remember.84a6e7b5.svg"
+  },
+  {
+    "revision": "ae27e9d26b6aa793729da00b35895f5c",
+    "url": "/INDUCTION_NEW/static/media/ribbon.ae27e9d2.svg"
+  },
+  {
+    "revision": "ab8b5df97bdf15598b19af497309731c",
+    "url": "/INDUCTION_NEW/static/media/sticky.ab8b5df9.svg"
+  },
+  {
+    "revision": "6461c8e22bacf3b02e445a06e8eeba4f",
+    "url": "/INDUCTION_NEW/static/media/sun-umbrella.6461c8e2.svg"
+  },
+  {
+    "revision": "b003dc445a2644f178124244293292b3",
+    "url": "/INDUCTION_NEW/static/media/target.b003dc44.svg"
+  },
+  {
+    "revision": "87bdc51dad35e28d81ea5fbfeb817506",
+    "url": "/INDUCTION_NEW/static/media/team.87bdc51d.svg"
+  },
+  {
+    "revision": "4fe5543e26f990f04b21c93e55fac88d",
+    "url": "/INDUCTION_NEW/static/media/tickets.4fe5543e.svg"
+  },
+  {
+    "revision": "a6d4d4c67f49aa0c906bea725d68d3a2",
+    "url": "/INDUCTION_NEW/static/media/time.a6d4d4c6.svg"
+  },
+  {
+    "revision": "0eb6897f6aec46c1f5a2f2a4240d7be2",
+    "url": "/INDUCTION_NEW/static/media/trophy.0eb6897f.svg"
+  },
+  {
+    "revision": "2f63dff9150dc0bd50745a65c16668e9",
+    "url": "/INDUCTION_NEW/static/media/welcome.2f63dff9.svg"
+  },
+  {
+    "revision": "707a603646c29cec6e682ef18043370e",
+    "url": "/INDUCTION_NEW/static/media/worldwide.707a6036.svg"
+  },
+  {
+    "revision": "2dad917621850da1a2e1f76e1aa9650c",
+    "url": "/INDUCTION_NEW/static/media/xebiaCares.2dad9176.png"
+  },
+  {
+    "revision": "9bcc3a0965fbf6b1b8696907872bce10",
+    "url": "/INDUCTION_NEW/static/media/xebiaCares.9bcc3a09.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/INDUCTION_NEW/precache-manifest.2e8652caf3e6e7df3d18f097c58db9af.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/INDUCTION_NEW/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});

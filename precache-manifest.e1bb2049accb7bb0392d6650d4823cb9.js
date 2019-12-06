b50441dca230d580bb74cdbde300886ee05788e7
sankalpnarayan27 <56334939+sankalpnarayan27@users.noreply.github.com>
2019-12-06 13:41:25 +0530

    Updates

--- precache-manifest.e1bb2049accb7bb0392d6650d4823cb9.js
+++ precache-manifest.e1bb2049accb7bb0392d6650d4823cb9.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2a0d74deae9d17e98cf3dc241567ae87",
+    "revision": "8ae397e860b6c6930c063fb19486093a",
     "url": "/INDUCTION_NEW/index.html"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/INDUCTION_NEW/precache-manifest.6a3d61a85a4f205f279b23250deb7d44.js"
+  "/INDUCTION_NEW/precache-manifest.e1bb2049accb7bb0392d6650d4823cb9.js"
 );
 
 self.addEventListener('message', (event) => {

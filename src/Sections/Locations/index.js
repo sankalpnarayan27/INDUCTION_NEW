57658e87f24f41295da0ccbd2bbd65466da8de1b
Govindan G <Govindan.G@XEBIAINDIA.LOCAL>
2020-02-03 22:05:40 +0530

    refactoring

--- src/Sections/Locations/index.js
+++ src/Sections/Locations/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 
+import SectionTitle from '../../Components/SectionTitle';
+
 import styles from './Locations.module.scss';
 
 import data from './locationsContent';
@@ -8,6 +10,11 @@ import data from './locationsContent';
 export default () => {
   return (
     <Row className={styles.locationsWrapper}>
+      <Col>
+        <SectionTitle
+          title="locations"
+        />
+      </Col>
       <Col className={styles.mapWrapper}>
       </Col>
       <Col>

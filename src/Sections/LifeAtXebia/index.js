ec8c5aff50e5670939fea68c77d5440763fc5a70
Govind G <govind@obscuredev.com>
2020-02-21 00:32:29 +0530

    removing unused code. updating packages

--- src/Sections/LifeAtXebia/index.js
+++ src/Sections/LifeAtXebia/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Row, Col, Container } from 'react-bootstrap';
 import Carousel, { Modal, ModalGateway } from 'react-images';
 import SectionTitle from '../../Components/SectionTitle';
